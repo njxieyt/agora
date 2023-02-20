0bbf5dda1d9c991c25fb13167988a3055d31ff3d
Tony <njxieyt@hotmail.com>
2023-02-20 22:10:27 +0800

    datatypes

--- migrations/2_deploy_agora.js
+++ migrations/2_deploy_agora.js
@@ -1,6 +1,7 @@
 const States = artifacts.require('States');
 const Errors = artifacts.require('Errors');
 const Calculate = artifacts.require('Calculate');
+const DataTypes = artifacts.require('DataTypes');
 const Agora = artifacts.require('Agora');
 const TradeLogic = artifacts.require('TradeLogic');
 
@@ -8,9 +9,11 @@ module.exports = async (deployer) => {
     deployer.deploy(States, { overwrite: true });
     deployer.deploy(Errors, { overwrite: true });
     deployer.deploy(Calculate, { overwrite: true });
+    deployer.deploy(DataTypes, { overwrite: true });
     deployer.link(States, TradeLogic);
     deployer.link(Calculate, TradeLogic);
     deployer.link(Errors, [TradeLogic, Agora]);
+    deployer.link(DataTypes, [TradeLogic, Agora]);
     deployer.deploy(TradeLogic, { overwrite: true });
     deployer.link(TradeLogic, Agora);
     deployer.deploy(Agora, { overwrite: true });
